43d9006746476148d082ed87502c07833073d969
Axel Jacobsen <axelnj44@gmail.com>
2020-09-21 23:46:00 +0100

    confident in correct reading of ADC; need to properly configure the ADC onn startup

--- Software/ads131m08_reader.py
+++ Software/ads131m08_reader.py
@@ -5,6 +5,27 @@ Application Notes:
        Raspberry Pis are only configured tor 8 bits per word. Therefore, we
        just send each byte in every word individually.
     2. Reading data after a pause: see 8.5.1.9.1
+
+The ADS131M08 performs all communication in SPI communication frames:
+
+=====
+8.5.1.7 SPI Communication Frames
+SPI communication on the ADS131M08 is performed in frames. Each SPI communication frame consists of several words.
+The word size is configurable as either 16 bits, 24 bits, or 32 bits by programming the WLENGTH[1:0] bits in the MODE register.
+-- NOTE FROM AXEL: We can configure this to what we want, but 24 bits seems to be the way to go. Since Raspberry Pi's are 8 bit,
+-- we have to do a bit of manual work splitting and combining bites to send over SPI
+
+The interface is full duplex, meaning that the interface is capable of transmitting data on DOUT while simultaneously receiving
+data on DIN. The input frame that the host sends on DIN always begins with a command. The first word on the output frame that
+the device transmits on DOUT always begins with the response to the command that was written on the previous input frame. The
+number of words in a command depends on the command provided. For most commands, there are ten words in a frame. On DIN, the
+host provides the command, the command CRC if input CRC is enabled or a word of zeros if input CRC is disabled, and eight
+additional words of zeros.
+
+Simultaneously on DOUT, the device outputs the response from the previous frame command, eight words of ADC data representing
+the eight ADC channels, and a CRC word.
+=====
+
 """
 
 import spidev
@@ -50,9 +71,10 @@ XTAL_OSC_DISABLE_MASK = 0b1 << 7
 EXTERNAL_REF_MASK = 0b0
 PWR_HIGH_RES_MASK = 0b11
 # mode register
-RESET_MASK = 0b0 << 10
+RESET_MASK = 0b1 << 10
 WLEN_24_MASK = 0b01 << 8
 SPI_TIMEOUT_MASK = 0b1 << 4
+DRDY_HIZ_OPEN_COLLECT = 0b1 << 1
 DRDY_FMT_PULSE_MASK = 0b1
 DRDY_FMT_LOW_MASK = 0b0
 # cgf register
@@ -68,10 +90,10 @@ class ADS131M09Reader(ADCReader):
         self.rpi_bits_per_word: int = 8  # only available word length on RPi
         self.bytes_per_word: int = int(self.ads_bits_per_word / self.rpi_bits_per_word)
 
-        self._DRDY: int = 37  # drdy pin is 37
+        self._DRDY: int = 31  # drdy pin is 37
         self._first_read: bool = True
 
-        GPIO.setup(self._DRDY, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self._DRDY, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def setup_adc(self, bus, device):
         self.spi.open(bus, device)
@@ -83,9 +105,11 @@ class ADS131M09Reader(ADCReader):
 
     def read(self):
         if self._first_read:
-            self.spi.xfer([0b0] * self.ads_num_frame_words * self.bytes_per_word)
+            self.spi.writebytes([0b0] * self.ads_num_frame_words * self.bytes_per_word)
             self._first_read = False
-        res = self.spi.xfer([0b0] * self.ads_num_frame_words * self.bytes_per_word)
+        command = [0b0] * self.ads_num_frame_words * self.bytes_per_word
+        self.spi.writebytes(command)
+        res = self.spi.readbytes(len(command))
         # Combine the bytes back into words before returning
         for i in range(1, 9):
             res[i] = twos_complement(res[i], 24)
@@ -106,12 +130,13 @@ class ADS131M09Reader(ADCReader):
         shift_value = self.ads_bits_per_word - 16
         register_shift = 7
 
-        shifted_data_payload = [*(data << shift_value).to_bytes(self.bytes_per_word, "little")]
+        shifted_data_payload = [*(data << shift_value).to_bytes(self.bytes_per_word, "big")]
         cmd = (WREG_OPCODE | (register_addr << register_shift) | (int(len(shifted_data_payload) / self.bytes_per_word) - 1)) << shift_value
-        cmd_payload = [*cmd.to_bytes(self.bytes_per_word, "little")]
+        cmd_payload = [*cmd.to_bytes(self.bytes_per_word, "big")]
         total_payload = cmd_payload + shifted_data_payload
         cmd_frame = total_payload + [0] * self.bytes_per_word * (10 - int(len(total_payload) / self.bytes_per_word))
-        return self.spi.xfer(cmd_frame)
+        self.spi.writebytes(cmd_frame)
+        return self.spi.readbytes(len(cmd_frame))
 
     def read_register(self, register_addr: int, num_registers: int = 1):
         """
@@ -128,9 +153,10 @@ class ADS131M09Reader(ADCReader):
         register_shift = 7
 
         cmd = (RREG_OPCODE | (register_addr << register_shift) | (num_registers - 1)) << shift_value
-        cmd_payload = [*cmd.to_bytes(self.bytes_per_word, "little")]
-        cmd_frame = cmd_payload #+ [0] * self.bytes_per_word * (10 - int(len(cmd_payload) / self.bytes_per_word))
-        return self.spi.xfer(cmd_frame)
+        cmd_payload = [*cmd.to_bytes(self.bytes_per_word, "big")]
+        cmd_frame = cmd_payload + [0] * self.bytes_per_word * (10 - int(len(cmd_payload) / self.bytes_per_word))
+        self.spi.writebytes(cmd_frame)
+        return self.spi.readbytes(len(cmd_frame))
 
     def data_ready(self):
         return not GPIO.input(adc_reader._DRDY)
@@ -143,21 +169,26 @@ class ADS131M09Reader(ADCReader):
         >>> _combine_bytes(0b01, 0b01)
         0000000100000001    
         """
-        out = 0
-        num_bytes = len(byte_args)
-        for i in range(num_bytes):
-            out |= byte_args[num_bytes - i - 1] << (8 * (num_bytes - i - 1))
-        return out
+        return int.from_bytes(byte_args, byteorder='big')
 
-def bits(v):
+    def bytes_to_readable(self, res):
+        return [bits(self._combine_bytes(*res[i:i + self.bytes_per_word])) for i in range(0, len(res), self.bytes_per_word)]
+
+
+def bits(v, word_len=24):
     bs = '{0:b}'.format(v)
-    bs = '0' * (4 - len(bs) % 4) + bs
+    if len(bs) % word_len == 0:
+        leading_zeros = 0
+    else:
+        leading_zeros = word_len - len(bs) % word_len
+    bs = '0' * leading_zeros + bs
     return ' '.join([bs[i:i+4] for i in range(0, len(bs), 4)])
 
 def twos_complement(input_value: int, num_bits: int) -> int:
     """Calculates a two's complement integer from the given input value's bits."""
     mask = 2 ** (num_bits - 1)
     return -(input_value & mask) + (input_value & ~mask)
+
 
 if __name__ == "__main__":
     adc_reader = ADS131M09Reader()
@@ -174,7 +205,7 @@ if __name__ == "__main__":
     # print('CLOCK WRITE', '{0:b}'.format(ALL_CH_DISABLE_MASK | OSR_16256_MASK | PWR_HIGH_RES_MASK | XTAL_OSC_DISABLE_MASK))
 
     # Clear reset flag, mmake DRDY active low, use 24 bit word length, & use a SPI Timeout
-    adc_reader.write_register(MODE_ADDR, RESET_MASK | DRDY_FMT_LOW_MASK | WLEN_24_MASK | SPI_TIMEOUT_MASK)
+    adc_reader.write_register(MODE_ADDR, RESET_MASK | DRDY_FMT_PULSE_MASK | DRDY_HIZ_OPEN_COLLECT | WLEN_24_MASK | SPI_TIMEOUT_MASK)
     # print('MODE WRITE', '{0:b}'.format(RESET_MASK | DRDY_FMT_PULSE_MASK | WLEN_24_MASK | SPI_TIMEOUT_MASK))
 
     # Enable Global Chop Mode, and rewrite default chop delay
@@ -185,9 +216,14 @@ if __name__ == "__main__":
     adc_reader.write_register(CLOCK_ADDR, CH_2_3_MASK | OSR_16256_MASK | PWR_HIGH_RES_MASK | XTAL_OSC_DISABLE_MASK)
     # print('CLOCK WRITE', '{0:b}'.format(CH_2_3_MASK | OSR_16256_MASK | PWR_HIGH_RES_MASK | XTAL_OSC_DISABLE_MASK))
 
-    print(adc_reader.read_register(ID_ADDR))
+    print('ID')
+    d = adc_reader.read_register(ID_ADDR)
+    print(adc_reader.bytes_to_readable(d))
+    print('STATUS')
+    d = adc_reader.read_register(STATUS_ADDR)
+    print(adc_reader.bytes_to_readable(d))
 
     while True:
         while not adc_reader.data_ready():
             pass
-        #print(adc_reader.read())
+        print(adc_reader.read())
