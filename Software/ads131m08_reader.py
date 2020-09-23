b251019201cc2d83847a42063462dd0c0df4c177
Axel Jacobsen <axelnj44@gmail.com>
2020-09-23 18:41:57 +0100

    minor modifications

--- Software/ads131m08_reader.py
+++ Software/ads131m08_reader.py
@@ -8,8 +8,7 @@ Application Notes:
 The ADS131M08 performs all communication in SPI communication frames:
 
 =====
-8.5.1.7 SPI Communication Frames
-SPI communication on the ADS131M08 is performed in frames. Each SPI communication frame consists of several words.
+8.5.1.7 SPI Communication Frames SPI communication on the ADS131M08 is performed in frames. Each SPI communication frame consists of several words.
 The word size is configurable as either 16 bits, 24 bits, or 32 bits by programming the WLENGTH[1:0] bits in the MODE register.
 -- NOTE FROM AXEL: We can configure this to what we want, but 24 bits seems to be the way to go. Since Raspberry Pi's are 8 bit,
 -- we have to do a bit of manual work splitting and combining bites to send over SPI
@@ -32,6 +31,7 @@ to accommodate 24- or 32-bit word sizes.
 """
 
 import spidev
+import time
 import RPi.GPIO as GPIO
 
 from adc_reader import ADCReader
@@ -42,14 +42,14 @@ GPIO.setmode(GPIO.BOARD)
 
 
 # Commands (ADS131M08 datasheet 8.5.1.10)
-WREG_OPCODE = 0b0110000000000000
-RREG_OPCODE = 0b1010000000000000
-UNLOCK_OPCODE = 0b0000011001010101
-LOCK_OPCODE = 0b0000010101010101
-WAKEUP_OPCODE = 0b0000000000110011
+WREG_OPCODE    = 0b0110000000000000
+RREG_OPCODE    = 0b1010000000000000
+UNLOCK_OPCODE  = 0b0000011001010101
+LOCK_OPCODE    = 0b0000010101010101
+WAKEUP_OPCODE  = 0b0000000000110011
 STANDBY_OPCODE = 0b0000000000100010
-RESET_OPCODE = 0b0000000000010001
-NULL_OPCODE = 0b0000000000000000
+RESET_OPCODE   = 0b0000000000010001
+NULL_OPCODE    = 0b0000000000000000
 
 
 # Register adresses
@@ -74,7 +74,10 @@ XTAL_OSC_DISABLE_MASK = 0b1 << 7
 EXTERNAL_REF_MASK = 0b0
 PWR_HIGH_RES_MASK = 0b11
 # mode register
+CRC_REG_EN_MASK = 0b1 << 13
+CRC_IN_EN_MASK = 0b1 << 12
 RESET_MASK = 0b1 << 10
+CLEAR_RESET_MASK = 0b0 << 10
 WLEN_24_MASK = 0b01 << 8
 SPI_TIMEOUT_MASK = 0b1 << 4
 DRDY_HIZ_OPEN_COLLECT = 0b1 << 1
@@ -141,6 +144,15 @@ class ADS131M09Reader(ADCReader):
         self.spi.writebytes(cmd_frame)
         return self.spi.readbytes(len(cmd_frame))
 
+    def write(self, cmd):
+        shift_value = self.ads_bits_per_word - 16
+        shifted_cmd = [*(cmd << shift_value).to_bytes(self.bytes_per_word, "big")]
+        cmd_frame = shifted_cmd + [0] * self.bytes_per_word * (10 - int(len(shifted_cmd) / self.bytes_per_word))
+
+        self.spi.writebytes(cmd_frame)
+        time.sleep(10e-6)
+        return self.spi.readbytes(len(cmd_frame))
+
     def read_register(self, register_addr: int, num_registers: int = 1):
         """
         params
@@ -179,7 +191,12 @@ class ADS131M09Reader(ADCReader):
 
 
 def bits(v, word_len=24):
-    bs = '{0:024b}'.format(v)
+    bs = '{0:b}'.format(v)
+    if len(bs) % word_len == 0:
+        zero_pads = 0
+    else:
+        zero_pads = word_len - len(bs) % word_len
+    bs = bs + '0' * zero_pads
     return ' '.join([bs[i:i+4] for i in range(0, len(bs), 4)])
 
 def twos_complement(input_value: int, num_bits: int) -> int:
@@ -197,41 +214,99 @@ if __name__ == "__main__":
         print("not ready")
     print("ready")
 
-    CH_2_3_MASK = ALL_CH_DISABLE_MASK | 1 << (8 + 2) | 1 << (8 + 3)
-    # Disable all channels so short frames can be written during config phase
-    adc_reader.write_register(CLOCK_ADDR, ALL_CH_DISABLE_MASK | OSR_16256_MASK | PWR_HIGH_RES_MASK | XTAL_OSC_DISABLE_MASK)
-    print('CLOCK WRITE', bits(ALL_CH_DISABLE_MASK | OSR_16256_MASK | PWR_HIGH_RES_MASK | XTAL_OSC_DISABLE_MASK))
-
-    # Clear reset flag, mmake DRDY active low, use 24 bit word length, & use a SPI Timeout
-    adc_reader.write_register(MODE_ADDR, RESET_MASK | DRDY_FMT_LOW_MASK | DRDY_HIZ_OPEN_COLLECT | WLEN_24_MASK | SPI_TIMEOUT_MASK)
-    print('MODE WRITE', bits(RESET_MASK | DRDY_FMT_LOW_MASK | DRDY_HIZ_OPEN_COLLECT | WLEN_24_MASK | SPI_TIMEOUT_MASK))
-
-    # Enable Global Chop Mode, and rewrite default chop delay
-    adc_reader.write_register(CFG_ADDR, GLOBAL_CHOP_EN_MASK | DEFAULT_CHOP_DELAY_MASK)
-    print('CFG WRITE', bits(GLOBAL_CHOP_EN_MASK | DEFAULT_CHOP_DELAY_MASK))
-
-    # Enable channels 2 and 3, and rewrite other desired settings
-    adc_reader.write_register(CLOCK_ADDR, CH_2_3_MASK | OSR_16256_MASK | PWR_HIGH_RES_MASK | XTAL_OSC_DISABLE_MASK)
-    print('CLOCK WRITE', bits(CH_2_3_MASK | OSR_16256_MASK | PWR_HIGH_RES_MASK | XTAL_OSC_DISABLE_MASK))
-
-    print('ID')
-    d = adc_reader.read_register(ID_ADDR)
+    print('RESET - should get 1111 1111 0010 1000')
+    d = adc_reader.write(RESET_OPCODE)
     print(adc_reader.bytes_to_readable(d))
 
-    print('STATUS')
+    print('initial STATUS register')
     d = adc_reader.read_register(STATUS_ADDR)
     print(adc_reader.bytes_to_readable(d))
+    print()
+
+    print('UNLOCK_OPCODE')
+    d = adc_reader.write(UNLOCK_OPCODE)
+    print(adc_reader.bytes_to_readable(d))
+    print()
+
+    print('STATUS register')
+    d = adc_reader.read_register(STATUS_ADDR)
+    print(adc_reader.bytes_to_readable(d))
+    print()
+
+    print('RESET opcode: expecting 1111 1111 0010 1000')
+    d = adc_reader.write(RESET_OPCODE)
+    print(adc_reader.bytes_to_readable(d))
+    print()
+
+    print('STATUS register')
+    d = adc_reader.read_register(STATUS_ADDR)
+    print(adc_reader.bytes_to_readable(d))
+    print()
+
+    print('UNLOCK opcode: expecting 0000 0110 0101 0101')
+    print(adc_reader.bytes_to_readable(adc_reader.write(UNLOCK_OPCODE)))
+    print()
+
+    print('STATUS register - first bit should be 0')
+    d = adc_reader.read_register(STATUS_ADDR)
+    print(adc_reader.bytes_to_readable(d))
+    print()
+
+    #CH_2_3_MASK = ALL_CH_DISABLE_MASK | 1 << (8 + 2) | 1 << (8 + 3)
+    # Disable all channels so short frames can be written during config phase
+    #clock_contents = ALL_CH_DISABLE_MASK | OSR_16256_MASK | PWR_HIGH_RES_MASK | XTAL_OSC_DISABLE_MASK
+    #adc_reader.write_register(CLOCK_ADDR, clock_contents)
+    #print('CLOCK WRITE', bits(clock_contents))
+    #print(adc_reader.bytes_to_readable(adc_reader.read_register(CLOCK_ADDR)))
 
     print('MODE')
     d = adc_reader.read_register(MODE_ADDR)
     print(adc_reader.bytes_to_readable(d))
 
-    print('CLOCK')
-    d = adc_reader.read_register(CLOCK_ADDR)
+    # Clear reset flag, mmake DRDY active low, use 24 bit word length, & use a SPI Timeout
+    mode_contents = CRC_REG_EN_MASK | CLEAR_RESET_MASK | DRDY_FMT_PULSE_MASK | DRDY_HIZ_OPEN_COLLECT | WLEN_24_MASK
+    adc_reader.write_register(MODE_ADDR, mode_contents)
+    print('MODE WRITE', bits(mode_contents))
+
+    print('MODE')
+    d = adc_reader.read_register(MODE_ADDR)
     print(adc_reader.bytes_to_readable(d))
+    print()
+
 
     print('CFG')
     d = adc_reader.read_register(CFG_ADDR)
+    print(adc_reader.bytes_to_readable(d))
+
+    # Enable Global Chop Mode, and rewrite default chop delay
+    cfg_contents = GLOBAL_CHOP_EN_MASK | DEFAULT_CHOP_DELAY_MASK
+    adc_reader.write_register(CFG_ADDR, cfg_contents)
+    print('CFG WRITE', bits(cfg_contents))
+
+    print('CFG')
+    d = adc_reader.read_register(CFG_ADDR)
+    print(adc_reader.bytes_to_readable(d))
+    print()
+
+    print('CLOCK')
+    d = adc_reader.read_register(CLOCK_ADDR)
+    print(adc_reader.bytes_to_readable(d))
+    # Enable channels 2 and 3, and rewrite other desired settings
+    clock_contents = ALL_CH_ENABLE_MASK | OSR_16256_MASK | PWR_HIGH_RES_MASK | XTAL_OSC_DISABLE_MASK
+    adc_reader.write_register(CLOCK_ADDR, clock_contents)
+    print('CLOCK WRITE', bits(clock_contents))
+
+    print('CLOCK')
+    d = adc_reader.read_register(CLOCK_ADDR)
+    print(adc_reader.bytes_to_readable(d))
+    print()
+
+    print('ID - should be 0010 1000 xxxx xxxx')
+    d = adc_reader.read_register(ID_ADDR)
+    print(adc_reader.bytes_to_readable(d))
+
+    print('STATUS - should be ')
+    d = adc_reader.read_register(STATUS_ADDR)
     print(adc_reader.bytes_to_readable(d))
 
     while True:
