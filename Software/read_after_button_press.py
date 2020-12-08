a12bb7cce3d0765dc74db20ea0be79d721fe31ff
paul-lebel <plebel@alumni.stanford.edu>
2020-12-08 15:07:33 -0800

    Update read_after_button_press.py
    - Move GPIO.add_event_detect after wait_for_edge
    - Delete unneeded doc string at top, residual from driver class

--- Software/read_after_button_press.py
+++ Software/read_after_button_press.py
@@ -6,26 +6,6 @@
 Datasheet
 	https://www.ti.com/lit/ds/symlink/ads131m08.pdf?ts=1601338573920
 
--- Application Notes --
-
-The SPI communication is done completely in 10-word frames (see 8.5.1.7 of the datasheet for information on this). Each
-word can be 2, 3, or 4 bytes (i.e. 16, 24, or 32 bits). The Raspberry Pi can only communicate 1-byte words. Therefore,
-if you were to attempt to send an integer which is outside the range of one byte, you would get an OS Error. This means
-we must split our words into individual bytes (which is done in our code by the int.to_bytes method). Since ADC data is
-24 bits (8.5.1.9), we will use 24-bit (3 byte) words.
-Communication words also have specific format that is worth knowing. Quoting from 8.5.1.8 SPI Communication Words:
-
-"The device defaults to a 24-bit word size. Commands, responses, CRC, and registers always contain 16 bits of actual
-data. These words are always most significant bit (MSB) aligned, and therefore the least significant bits (LSBs) are
-zero-padded to accommodate 24- or 32-bit word sizes."
-
-So in short, each of our words that we receive or send which are only 16 bytes long are zero-padded on the right.
-So if 'd' signifies a data bit, each word of commands, responses, CRC, and registers have the format (spaces
-added for clarity)
-
-	dddd dddd dddd dddd 0000 0000
-
-This is responsible for some bit shifting which occurs throughout the document.
 """
 
 import time
@@ -105,13 +85,15 @@ if __name__ == "__main__":
 				return
 			print(f"CH0: {d[0]} \t CH1: {d[1]}")
 
-		GPIO.add_event_detect(adc_reader._DRDY, GPIO.FALLING, callback=cb)
 
 		# Wait for button-press,then wait a delay before starting the measurement
 		GPIO.setup(BTN_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 		GPIO.wait_for_edge(BTN_1, GPIO.FALLING)
 		print(f'Button pressed. Waiting for {delayTime_seconds} seconds...')
 		time.sleep(delayTime_seconds)
+
+		# Start logging incoming data
+		GPIO.add_event_detect(adc_reader._DRDY, GPIO.FALLING, callback=cb)
 
 		t0 = time.time()
 		try:
