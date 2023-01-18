6aaaa91caa622ceecc1ac60200e924ec41c0d244
Paul Lebel <30475643+paul-lebel@users.noreply.github.com>
2023-01-18 09:50:21 -0800

    Update setup.py
    - Add minimum versions
    - Remove RPi.GPIO as it is not a pip-installed package

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ import setuptools
 setuptools.setup(
     name="luminometer",
     version="1.0.0",
+    python_requires='>=3.7',
     author="Paul Lebel",
     author_email="plebel@alumni.stanford.edu",
     description="Software to control a low-cost, high-sensitivity luminometer.",
     url="https://github.com/czbiohub/ulc-tube-reader",
     install_requires=[
-        'Pillow',
-        'inky',
-        'numpy',
-        'pigpio',
-        'RPi.GPIO'
+        'Pillow>=8.3.1',
+        'inky>=1.2.2',
+        'numpy>=1.21.1',
+        'pigpio>=1.78'
     ],
     classifiers=[
         "CZ Biohub :: Bioengineering",
